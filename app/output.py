8aa91ebf6d39e7a09bcb8288e059a9e218cb24a7
Jean-Baptiste Louazel <jb.louazel@gmail.com>
2017-02-23 20:21:05 +0100

    Fix output
    Signed-off-by: Jean-Baptiste Louazel <jb.louazel@gmail.com>

--- app/output.py
+++ app/output.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python3
 
-def get_used_caches_count(caches_list):
+def get_used_caches_count(cache_list):
     caches_used = 0
 
     for cache in cache_list:
-        if cache.max_size != cache.available_size:
+        if cache_list[cache].max_size != cache_list[cache].available_size:
             caches_used += 1
 
     return caches_used
 
 
 def format_output(cache_list):
-    output_str = str(get_used_caches_count(caches_list)) + "\n"
+    output_str = str(get_used_caches_count(cache_list)) + "\n"
 
     for cache in cache_list:
-        if cache.max_size != cache.available_size:
+        if cache_list[cache].max_size != cache_list[cache].available_size:
             output_str += str(cache.id)
 
-            for video in cache.videos:
-                output_str += " " + str(video.id)
+            for video in cache_list[cache].videos:
+                output_str += " " + str(cache_list[cache].videos[video].id)
 
             output_str += "\n"
 
