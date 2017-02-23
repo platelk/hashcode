837b637003a06b7315ba02ee6eaa1cd61780d541
Jean-Baptiste Louazel <jb.louazel@gmail.com>
2017-02-23 19:36:41 +0100

    Add output formating func
    Signed-off-by: Jean-Baptiste Louazel <jb.louazel@gmail.com>

---
+++ app/output.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+def get_used_caches_count(caches_list):
+    caches_used = 0
+
+    for cache in cache_list:
+        if cache.max_size != cache.available_size:
+            caches_used += 1
+
+    return caches_used
+
+
+def format_output(cache_list):
+    output_str = str(get_used_caches_count(caches_list)) + "\n"
+
+    for cache in cache_list:
+        if cache.max_size != cache.available_size:
+            output_str += str(cache.id)
+
+            for video in cache.video_list:
+                output_str += " " + str(video.id)
+
+            output_str += "\n"
+
+    return output_str
